72dbb4ac271a01658e0419273a076c67cfb99a25
zorbaTheRainy <github2@scout-family.org>
2024-10-06 22:48:04 +0200

    added `mkdir -p /var/run/s6/etc`

--- dnsmasq_run.sh
+++ dnsmasq_run.sh
@@ -1,2 +1,7 @@
 #!/command/execlineb -P
+
+# Ensure the necessary directories exist
+mkdir -p /var/run/s6/etc
+
+# Run the webproc command with the specified configuration
 webproc --configuration-file /etc/dnsmasq.conf -- dnsmasq --no-daemon
