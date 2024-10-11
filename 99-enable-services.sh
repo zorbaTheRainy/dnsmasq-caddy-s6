0dbdd66b6600d11e13a6575efbccfa334a593b0f
zorbaTheRainy <github2@scout-family.org>
2024-10-11 22:53:27 +0200

    I think git messed up

--- 99-enable-services.sh
+++ 99-enable-services.sh
@@ -1,43 +1,32 @@
-#!/command/execlineb -P
-with-contenv
+#!/bin/bash
 
+    # -------------------------------------------------------------------------------------------------
+    # Caddy docker image     ->  https://hub.docker.com/_/caddy
 # -------------------------------------------------------------------------------------------------
-# Caddy docker image     ->  https://hub.docker.com/_/caddy
-# -------------------------------------------------------------------------------------------------
 
-s6-echo "Enable: ${ENABLE_CADDY}"
+echo "Enable: ${ENABLE_CADDY}"
 
-# Debugging: Check if ENABLE_CADDY is set
-if { s6-test ${ENABLE_CADDY} = "" }
-    s6-echo "ENABLE_CADDY is not set!"
-    exit 1
-
-# Check if ENABLE_CADDY is 1 or 0 and set the appropriate value
-if { s6-test ${ENABLE_CADDY} -eq 1 }
-    foreground {
-        s6-echo "ENABLE_CADDY = true"
-        s6-export ENABLE_CADDY true
-    }
-if { s6-test ${ENABLE_CADDY} -eq 0 }
-    foreground {
-        s6-echo "ENABLE_CADDY = false"
-        s6-export ENABLE_CADDY false
-    }
+# Check if ENABLE_CADDY is 1 or true (case insensitive)
+if [ "${ENABLE_CADDY}" -eq 1 ] || [ "${ENABLE_CADDY}" -eq 0 ] 2>/dev/null; then
+  if [ "${ENABLE_CADDY}" -eq 1 ]; then
+    ENABLE_CADDY="true"
+  else
+    ENABLE_CADDY="false"
+  fi
+fi
 
 # Convert ENABLE_CADDY to lowercase for comparison
-foreground {
-    ENABLE_CADDY_LOWER $(echo ${ENABLE_CADDY} | tr '[:upper:]' '[:lower:]')
+ENABLE_CADDY_LOWER=$(echo "${ENABLE_CADDY}" | tr '[:upper:]' '[:lower:]')
 
-    # Perform the actual check
-    if { s6-test ${ENABLE_CADDY_LOWER} = "true" }
-        s6-echo '[run] enabling Caddy reverse proxy'
+# perform the actual check
+if [ "${ENABLE_CADDY_LOWER}" == "true" ]; then
+  echo '[run] enabling Caddy reverse proxy'
 
-        if { s6-test -d /etc/services.d/caddy }
-            s6-echo '[run] Caddy reverse proxy already enabled'
-        else
-            ln -s /etc/services-available/caddy /etc/services.d/caddy
-        fi
-    fi
-
-    rm /etc/services.d/99-enable-services/run
-}
+  # Enable nginx as a supervised service
+  if [ -d /etc/services.d/caddy ]
+  then
+    echo '[run] Caddy reverse proxy already enabled'
+  else
+    ln -s /etc/services-available/caddy /etc/services.d/caddy
+  fi
+fi
