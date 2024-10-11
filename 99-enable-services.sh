db2e83e5f917829977461f30262ab5fe2ed8f17a
zorbaTheRainy <github2@scout-family.org>
2024-10-11 23:44:45 +0200

    removed s6-envdir

--- 99-enable-services.sh
+++ 99-enable-services.sh
@@ -1,37 +1,37 @@
 #!/command/execlineb -P
 with-contenv
 
-s6-envdir /etc/envs
-s6-echo Enable: ${ENABLE_CADDY}
+# -------------------------------------------------------------------------------------------------
+# Caddy docker image     ->  https://hub.docker.com/_/caddy
+# -------------------------------------------------------------------------------------------------
+
+s6-echo "Enable: ${ENABLE_CADDY}"
 
 # Debugging: Check if ENABLE_CADDY is set
 if { s6-test ${ENABLE_CADDY} = "" }
-    foreground {
-      s6-echo ENABLE_CADDY is not set!
-      exit 1
-    }
+    s6-echo "ENABLE_CADDY is not set!"
+    exit 1
 
-# Check if ENABLE_CADDY is 1 or true (case insensitive)
+# Check if ENABLE_CADDY is 1 or 0 and set the appropriate value
 if { s6-test ${ENABLE_CADDY} -eq 1 }
     foreground {
-        s6-envdir /etc/envs s6-echo ENABLE_CADDY = true
+        s6-echo "ENABLE_CADDY = true"
         s6-export ENABLE_CADDY true
     }
-
 if { s6-test ${ENABLE_CADDY} -eq 0 }
     foreground {
-        s6-envdir /etc/envs s6-echo ENABLE_CADDY = false
+        s6-echo "ENABLE_CADDY = false"
         s6-export ENABLE_CADDY false
     }
 
+# Convert ENABLE_CADDY to lowercase for comparison
 foreground {
-    # Convert ENABLE_CADDY to lowercase for comparison
-    s6-envdir /etc/envs ENABLE_CADDY_LOWER $(echo ${ENABLE_CADDY} | tr '[:upper:]' '[:lower:]')
+    ENABLE_CADDY_LOWER $(echo ${ENABLE_CADDY} | tr '[:upper:]' '[:lower:]')
 
     # Perform the actual check
     if { s6-test ${ENABLE_CADDY_LOWER} = "true" }
         s6-echo '[run] enabling Caddy reverse proxy'
-        
+
         if { s6-test -d /etc/services.d/caddy }
             s6-echo '[run] Caddy reverse proxy already enabled'
         else
